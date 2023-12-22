63d21ed565a5ed50c688615503d51403843d4207
Abraham Peter <47416051+theclassical-dev@users.noreply.github.com>
2023-12-22 13:22:24 +0100

    app_theme, color pallete and theme added

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:livescore/theme/theme.dart';
+
+class AppTheme {
+  static ThemeData theme = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: Pallete.backgroundColor,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Pallete.backgroundColor,
+      elevation: 0,
+    ),
+  );
+}
---
+++ lib/theme/pallete.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Pallete {
+  static const Color backgroundColor = Color(0xFFe1ebe9);
+  static const Color whiteColor = Colors.white;
+  static const Color blueColor = Color(0xFF232259);
+  static const Color redColor = Color(0xFF912617);
+  static const Color greyColor = Colors.grey;
+}
