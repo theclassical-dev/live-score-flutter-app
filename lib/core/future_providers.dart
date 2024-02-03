6941b46f07047acbe94c06a3e1791934a1ead162
Abraham Peter <47416051+theclassical-dev@users.noreply.github.com>
2024-02-03 16:47:03 +0100

    matchListprovider created added

--- lib/core/future_providers.dart
+++ lib/core/future_providers.dart
@@ -1,7 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:livescore/core/core.dart';
 import 'package:livescore/models/leagues.dart';
+import 'package:livescore/models/match.dart';
 
 final leagueListProvider = FutureProvider<List<League>>((ref) async {
   return ref.watch(apiServiceProvider).getLeagues();
+});
+
+final matchListProvider = FutureProvider<List<MatchModel>>((ref) async {
+  return ref.watch(apiServiceProvider).getMatches();
 });
