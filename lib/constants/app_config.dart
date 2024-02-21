b6904e3f477ad97318d6ef12f0069a59836b2122
Abraham Peter <47416051+theclassical-dev@users.noreply.github.com>
2024-02-21 20:36:35 +0100

    newsbaseur + apiKey added

--- lib/constants/app_config.dart
+++ lib/constants/app_config.dart
@@ -1,6 +1,9 @@
 class AppUrlConstants {
   static const String baseURL = "http://api.football-data.org/v4";
   static const String token = "0eb806f001ae4d7eb7c6e1675c0299d1";
+  static const String newsBaseURL =
+      "https://newsapi.org/v2/everything?domains=skysports.com";
+  static const newsApiKey = "4ff2de299df744079b1466b94a27149b";
   //filter filter must not pass 10days
   // https://api.football-data.org/v4/matches/?dateFrom=2024-01-31&dateTo=2024-02-10
   //finished filter matches with data
